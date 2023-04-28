49739a6611de702c5aa8a2150b7adcca6737e83e
Dzmitry Kryvialeu <111528094+dfkrivelev@users.noreply.github.com>
2023-04-28 18:50:49 +0200

    create index.html

--- src/main/java/com/minibank/controllers/StartController.java
+++ src/main/java/com/minibank/controllers/StartController.java
@@ -1,4 +1,23 @@
 package com.minibank.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class StartController {
+
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        return "main/index";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage(Model model) {
+        return "about";
+    }
+
+
 }
