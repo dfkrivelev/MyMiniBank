66136794b937f50703d7e27cc954bb15ea210a06
Dzmitry Kryvialeu <111528094+dfkrivelev@users.noreply.github.com>
2023-05-14 19:33:20 +0200

    account list ok

--- src/main/java/com/minibank/controllers/AdminController.java
+++ src/main/java/com/minibank/controllers/AdminController.java
@@ -1,6 +1,9 @@
 package com.minibank.controllers;
 
+import com.minibank.models.Account;
 import com.minibank.models.User;
+import com.minibank.services.AccountService;
+import com.minibank.services.TransactionService;
 import com.minibank.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -9,13 +12,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/admin")
 
 public class AdminController {
 
+
+    private final UserService userService;
+    private final AccountService accountService;
+    private final TransactionService transactionService;
+
     @Autowired
-    private UserService userService;
+    public AdminController(UserService userService, AccountService accountService, TransactionService transactionService) {
+        this.userService = userService;
+        this.accountService = accountService;
+        this.transactionService = transactionService;
+    }
 
     @GetMapping("/main")
     public String mainPage(Model model) {
@@ -29,9 +43,18 @@ public class AdminController {
     public String userListPage(Model model) {
         User user = userService.getAuthUser();
 
+        model.addAttribute("accounts", accountService.findAll());
         model.addAttribute("users", userService.findAll());
         model.addAttribute("user", user);
         return "admin/userList";
     }
 
+    @GetMapping("/accountList")
+    public String accountListPage(Model model) {
+        User user = userService.getAuthUser();
+
+        model.addAttribute("accounts", accountService.findAll());
+        model.addAttribute("user", user);
+        return "admin/accountList";
+    }
 }
