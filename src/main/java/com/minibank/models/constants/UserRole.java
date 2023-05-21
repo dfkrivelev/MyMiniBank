3d20b269582645b8e5f0bcfd6ff9db1fe07b343d
Dzmitry Kryvialeu <111528094+dfkrivelev@users.noreply.github.com>
2023-05-21 17:56:50 +0200

    add dependency for swagger

--- src/main/java/com/minibank/models/constants/UserRole.java
+++ src/main/java/com/minibank/models/constants/UserRole.java
@@ -8,18 +8,4 @@ import java.util.stream.Collectors;
 public enum UserRole {
     ADMIN,
     CLIENT
-//    private final Set<Permission> permissions;
-//    UserRole(Set<Permission> permissions) {
-//        this.permissions = permissions;
-//    }
-//
-//    public Set<Permission> getPermissions() {
-//        return permissions;
-//    }
-//
-//    public Set<SimpleGrantedAuthority> getAuthorities() {
-//        return getPermissions().stream()
-//                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-//                .collect(Collectors.toSet());
-//    }
 }
